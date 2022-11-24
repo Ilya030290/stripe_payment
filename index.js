9c63fd6d7eae9e15cdab2754ae6a3d729b9fd9d2
Ilya030290 <anoshko90@mail.ru>
2022-11-24 11:43:51 +0300

    fix: fix index.js with new functionality

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config({ path: './.env' });
-const port = 8000;
-
-const router = require('./routes/checkout');
-
 const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config({ path: './.env' });
+const port = process.env.PORT;
+const mongoDBConnect = require('./mongoDBConnect');
+
+const checkoutRouter = require('./routes/checkout');
+const shopItemsRouter = require('./routes/shopItems');
+const categoriesRouter = require('./routes/categories');
+const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: true }));
+app.use(cors({
+  origin: process.env.WEB_APP_URL,
+  credentials: true,
+  optionsSuccessStatus: 200,
+}));
 
-app.use('/create-checkout-session', router);
+app.use(cookieParser());
+
+mongoDBConnect();
+
+app.use(
+  checkoutRouter,
+  categoriesRouter,
+  shopItemsRouter,
+  authRouter,
+  usersRouter
+);
 
 app.listen(port, () => {
-    console.log(`Server start on port: ${port}`)
-})
+  console.log(`Server start on port: ${port}`);
+});
