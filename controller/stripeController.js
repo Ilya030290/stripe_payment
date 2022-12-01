0a07b740422f139d9ec3113bacec221c0620e373
Ilya030290 <anoshko90@mail.ru>
2022-12-01 16:33:34 +0300

    fix: fix base url for stripe

--- controller/stripeController.js
+++ controller/stripeController.js
@@ -1,7 +1,7 @@
 const stripeApi = require('./../stripe');
 
 async function createCheckoutSession(req, res) {
-  const domainUrl = process.env.WEB_APP_URL;
+  const domainUrl = 'https://threejs-shop-production.up.railway.app';
   const { line_items, customer_email } = req.body;
   if (!line_items || !customer_email) {
     return res.status(400).json({ error: 'missing required session parameters' });
